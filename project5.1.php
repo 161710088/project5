279cc0598cd15289fbf63358ae4deffb9a470b19
Lab RPL 1 <161710114@smkassalaam.sch.id>
2017-10-25 17:06:48 +0400

    project5

---
+++ project5.1.php
@@ -0,0 +1,8 @@
+<?php
+require_once "project5.php";
+
+$ab = new suara ('mbeeee','petok petok petok','meong');
+echo "suara domba ".$ab->domba.'<br>';
+echo "suara ayam ".$ab->ayam.'<br>';
+echo "suara kucing ".$ab->kucing.'<br>';
+?>
---
+++ project5.php
@@ -0,0 +1,14 @@
+<?php
+class suara{
+ 		public $domba;
+ 		public $ayam;
+ 		public $kucing;
+ 		//KONSTRAKTOR
+ 		public function __construct($a,$b,$c)
+ 		{
+			$this->domba = $a;
+			$this->ayam = $b;
+			$this->kucing = $c;
+ 		}
+}
+?>
